78f56d90063262d8fe225b724f674d9c061a2b08
nagi <nagi>
2022-02-19 20:23:09 +0000

    added service for demos

--- services/python-demo-setup/demo_setup.py
+++ services/python-demo-setup/demo_setup.py
@@ -1,16 +1,26 @@
 import time
 import logging
 import nipyapi
+import requests
 
 logging.basicConfig(level=logging.DEBUG)
 
-nipyapi.config.nifi_config.host = 'http://localhost:8091/nifi-api'
-nipyapi.config.registry_config.host = 'http://localhost:18080/nifi-registry-api'
+nipyapi.config.nifi_config.host = "http://nifi:8080/nifi-api"
+nipyapi.config.registry_config.host = "http://nifi-reg:18080/nifi-registry-api"
 
-sleep_secs = 10
-logging.info(f"sleeping for {sleep_secs} seconds")
-time.sleep(10)
-logging.info(f"woke up after {sleep_secs} seconds")
+status_code = 0
+status_code_reg = 0
+while status_code != 200 or status_code_reg != 200:
+    try:
+        status_code_reg = requests.get(nipyapi.config.registry_config.host + "/access").status_code
+        logging.warning(f"status_code on /nifi-registry-api/access {status_code_reg}")
+        status_code = requests.get(nipyapi.config.nifi_config.host + "/system-diagnostics").status_code
+        logging.warning(f"status_code on /nifi-api/system-diagnostics {status_code}")
+    except Exception as e:
+        logging.warning(str(e))
+    time.sleep(5)
+
+logging.warning(f"Starting NiPyApi demo run")
 # demo and api calls run by import
 from nipyapi.demo.console import *
-dir()
+logging.warning(f"demo files created: {dir()}")
